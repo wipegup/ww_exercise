5a2ccaf2039144c8cb47e98cd4908ed5e4c12a6e
wipegup <wipegup@gmail.com>
2019-07-18 10:27:42 -0600

    Getting and displaying top word

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 import $ from 'jquery'
 
-$(document).ready(() => {
+function displayTopWord(response){
+  console.log("displaying");
+  $('article.word-count').empty()
+  let wordInfo =  response['word'];
+  let word = Object.keys(wordInfo)[0];
+  let count = wordInfo[word];
+  $('article.word-count').prepend(`<div>${word} : ${count}</div>`)
+}
+
+function getTopWord(){$.ajax({
+  type:"GET",
+  url: "https://wordwatch-api.herokuapp.com/api/v1/top_word",
+  success: displayTopWord
+})};
+
+$(document).ready( () => {
+
+  getTopWord();
+
+  // $('button').on('click', postWordList)
   // have fun!
+
+
 })
